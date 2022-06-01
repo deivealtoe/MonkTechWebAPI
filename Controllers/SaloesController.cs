faa811a02c10554c3810d864ecfb3c1055c4ccf8
Deivisson Altoe <deivealtoe@gmail.com>
2022-05-31 21:28:49 -0300

    Removido namespaces não mais utilizados que estavam causando problemas

--- Controllers/SaloesController.cs
+++ Controllers/SaloesController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MonkTechWebAPI.Contracts;
-using MonkTechWebAPI.Data;
 using MonkTechWebAPI.Models;
 using MonkTechWebAPI.Models.Dto.Salao;
 
